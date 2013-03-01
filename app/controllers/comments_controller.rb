7a020b8bc05743ff687a063f8753165cc05e7ac3
keepsleeping <satbirdd@gmail.com>
2013-03-01 23:28:15 +0800

    travis

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -47,7 +47,7 @@ class CommentsController < ApplicationController
 
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
+        format.html { redirect_to @post, notice: 'Comment was successfully created.' }
         format.json { render json: @comment, status: :created, location: @comment }
       else
         format.html { render action: "new" }
